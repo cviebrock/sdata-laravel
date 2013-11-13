74990a5a36d35e736993df3dd5e3311dc621888a
Colin Viebrock <colin@viebrock.ca>
2013-11-12 20:24:47 -0600

    add club lookup

--- src/Golfcanada/Golfnet/Golfnet.php
+++ src/Golfcanada/Golfnet/Golfnet.php
@@ -11,4 +11,13 @@ class Golfnet {
 		$this->sdata = $sdata;
 	}
 
+
+	function getClub( $id )
+	{
+		$req = $this->sdata->get('accounts("'.$id.'")');
+		$rsp = $req->send();
+		return $req->json();
+	}
+
+
 }
